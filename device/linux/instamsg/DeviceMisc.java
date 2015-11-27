1059f76def2e95baa750b7337ddf2b1a24c465c4
Ajay Garg <ajay.garg@sensegrow.com>
2015-11-27 20:10:46 +0530

    Using the wlan0-mac-address as the UUID for linux-device.

--- device/linux/instamsg/DeviceMisc.java
+++ device/linux/instamsg/DeviceMisc.java
@@ -1,12 +1,35 @@
 package device.linux.instamsg;
 
+import java.net.NetworkInterface;
+import java.net.SocketException;
+
 import common.instamsg.driver.Misc;
 
 public class DeviceMisc implements Misc {
 
 	@Override
 	public String getDeviceUuid() {
-		return "ajay-client-100";
-	}
+	
+		/*
+		 * For our device.. we form a UUID using the laptop's wlan0 interface MAC-address.
+		 */
+		try {
 
+			NetworkInterface network = NetworkInterface.getByName("wlan0");
+			byte[] mac = network.getHardwareAddress();
+
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < mac.length; i++) {
+				sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
+			}
+			
+			return "LINUX-DESKTOP:ETH0:MAC:" + sb.toString();
+
+		} catch (SocketException e) {
+			e.printStackTrace();
+			
+		}
+		
+		return null;
+	}
 }
