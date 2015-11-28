767226a3c7781e4ca6e358369cf1722d1026662e
Ajay Garg <ajay.garg@sensegrow.com>
2015-11-28 22:42:05 +0530

    Adding missing-methods in watchdog-interfaces.

--- common/instamsg/driver/Watchdog.java
+++ common/instamsg/driver/Watchdog.java
@@ -2,4 +2,6 @@ package common.instamsg.driver;
 
 public interface Watchdog {
 	void watchdogInit();
+	void watchdogResetAndEnable(int n, String callee);
+	public void watchdogDisable();
 }
--- device/linux/instamsg/DeviceWatchdog.java
+++ device/linux/instamsg/DeviceWatchdog.java
@@ -1,13 +1,48 @@
 package device.linux.instamsg;
 
-
 import common.instamsg.driver.Watchdog;
 
 public class DeviceWatchdog implements Watchdog {
 
+	
+	/**
+	 * This method initializes the watchdog-timer.
+	 */
 	@Override
 	public void watchdogInit() {
 
 	}
-
+	
+	
+	/**
+	 * This method resets the watchdog-timer.
+	 *
+	 * Once this is completed, the watchdog-timer starts counting down from "n" seconds to 0.
+	 * Then either of the following must happen ::
+	 *
+	 * a)
+	 * Counter reaches 0.
+	 *
+	 * The device must then be reset/restarted.
+	 *
+	 * b)
+	 * "watchdogDisable()" is called.
+	 *
+	 * In this case, the countdown-timer stops, and the device must never be reset/restarted (until the entire
+	 * "watchdogResetAndEnable" loop is repeated).
+	 *
+	 */
+	@Override
+	public void watchdogResetAndEnable(int n, String callee) {
+		
+	}
+	
+	
+	/**
+	 * This method disables the watchdog-timer.
+	 */
+	@Override
+	public void watchdogDisable() {
+		
+	}
 }
